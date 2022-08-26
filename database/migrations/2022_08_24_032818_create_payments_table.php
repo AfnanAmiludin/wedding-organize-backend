1dac4293c86a1aca0930d970166a5f8336af0e87
Afnan Amiludin <amldn.afnn@gmail.com>
2022-08-26 07:59:45 +0700

    migration in database

---
+++ database/migrations/2022_08_24_032818_create_payments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_client');
+            $table->string('total_cash');
+            $table->string('terbayar');
+            $table->string('keterangan');
+            $table->date('tanggal');
+            $table->bigInteger('wo_id');
+            $table->softDeletes(); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+};
---
+++ database/migrations/2022_08_24_034937_create_jadwals_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jadwals', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_activity');
+            $table->string('detail_activity');
+            $table->string('name_client');
+            $table->date('tanggal');
+            $table->time('jam');
+            $table->bigInteger('wo_id');
+            $table->bigInteger('member_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jadwals');
+    }
+};
